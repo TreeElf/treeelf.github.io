e3be433f17a9cf98227a03343fbaa62a2b7a3cd9
TreeElf <32620629+TreeElf@users.noreply.github.com>
2018-01-12 13:23:04 -0600

    added css background logic

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -24,40 +24,94 @@ function createCharacter() {
   	character.stamina = document.getElementById('stamina').value;
   	character.mana = document.getElementById('mana').value;
   	character.hp = document.getElementById('hp').value;
+	var elem = document.getElementById("charactercreation");
+	elem.remove();
 	alert(character.name + " " + character.attack + " " + character.defense + " " + character.stamina + " " + character.mana + " " + character.hp);
 }
-var skillpoints = 50;
+var sp = 10;
+skillpoints.innerHTML = "Skill Points Left: "  + sp;
 var a = document.getElementById("attack");
 var av = document.getElementById("attackv");
 av.innerHTML = "Attack - " + a.value; 
 attack.oninput = function() {
-    if(skillpoints>av.value)
-    av.innerHTML = "Attack - " + this.value;
+	var old = av.innerHTML.match(/\d+/g).map(Number);
+	if((sp-(this.value-old))>=0) {
+		sp+=old-this.value
+		av.innerHTML = "Attack - " + this.value;
+	} else {
+		var l = sp+Number(old)
+		a.value = l;
+		av.innerHTML = "Attack - " + a.value;
+		sp=0
+	}
+    skillpoints.innerHTML = "Skill Points Left: "  + sp;
 }
 var b = document.getElementById("defense");
 var bv = document.getElementById("defensev");
 bv.innerHTML = "Defense - " + b.value; 
 defense.oninput = function() {
-    bv.innerHTML = "Defense - " + this.value;
+    var old = bv.innerHTML.match(/\d+/g).map(Number);
+	if((sp-(this.value-old))>=0) {
+		sp+=old-this.value
+		bv.innerHTML = "Defense - " + this.value;
+	} else {
+		var l = sp+Number(old)
+		b.value = l;
+		bv.innerHTML = "Defense - " + b.value;
+		sp=0
+	}
+    skillpoints.innerHTML = "Skill Points Left: "  + sp;
 }
 var c = document.getElementById("stamina");
 var cv = document.getElementById("staminav");
 cv.innerHTML = "Stamina - " + c.value; 
 stamina.oninput = function() {
-    cv.innerHTML = "Stamina - " + this.value;
+	var old = cv.innerHTML.match(/\d+/g).map(Number);
+	if((sp-(this.value-old))>=0) {
+		sp+=old-this.value
+		cv.innerHTML = "Stamina - " + this.value;
+	} else {
+		var l = sp+Number(old)
+		c.value = l;
+		cv.innerHTML = "Stamina - " + c.value;
+		sp=0
+	}
+    skillpoints.innerHTML = "Skill Points Left: "  + sp;
 }
 var d = document.getElementById("mana");
 var dv = document.getElementById("manav");
 dv.innerHTML = "Mana - " + d.value; 
 mana.oninput = function() {
-    dv.innerHTML = "Mana - " + this.value;
+    var old = dv.innerHTML.match(/\d+/g).map(Number);
+	if((sp-(this.value-old))>=0) {
+		sp+=old-this.value
+		dv.innerHTML = "Mana - " + this.value;
+	} else {
+		var l = sp+Number(old)
+		d.value = l;
+		dv.innerHTML = "Mana - " + d.value;
+		sp=0
+	}
+    skillpoints.innerHTML = "Skill Points Left: "  + sp;
+    
 }
 var e = document.getElementById("hp");
 var ev = document.getElementById("hpv");
 ev.innerHTML = "HP - " + e.value; 
 hp.oninput = function() {
-    ev.innerHTML = "HP - " + this.value;
+    var old = ev.innerHTML.match(/\d+/g).map(Number);
+	if((sp-(this.value-old))>=0) {
+		sp+=old-this.value
+		ev.innerHTML = "HP - " + this.value;
+	} else {
+		var l = sp+Number(old)
+		e.value = l;
+		ev.innerHTML = "HP - " + e.value;
+		sp=0
+	}
+    skillpoints.innerHTML = "Skill Points Left: "  + sp;
+
 }
-var txt="some text 2";
-var numb = txt.match(/\d/g);
-alert(numb);
+//var txt="some text 20";
+//var numb = txt.match(/\d+/g).map(Number);
+//alert(numb);
